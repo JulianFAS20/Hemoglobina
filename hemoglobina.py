2a4552442853fc45bbd2b566a7e42955fe687a7d
Julian Avila <julian.avila@datatools.com.co>
2022-05-24 23:29:36 -0500

    Adición del proyecto de hemoglobina al repo

---
+++ hemoglobina.py
@@ -0,0 +1,20 @@
+homo = float(input())
+genero = int(input())
+
+if genero == 1 or genero == 2:
+    if genero == 1:
+        if homo  <  13.2:
+            print(f'Alerta 1')
+        elif homo >= 13.2 and homo <= 16.6:
+            print(f'Sin alerta')
+        elif homo > 16.6:
+            print(f'Alerta 2')
+    else:
+        if homo < 11.6:
+            print(f'Alerta 1')
+        elif homo >= 11.6 and homo <= 15.0:
+            print(f'Sin alerta')
+        elif homo  > 15.0:
+            print(f'Alerta 2')
+else:
+    print(f'No es posible generar la alerta')
