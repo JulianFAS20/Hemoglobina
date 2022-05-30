7d448d766274993cb63db606040dcf7cbe6638cc
TzJuanSanger <79180600+TzJuanSanger@users.noreply.github.com>
2022-05-29 21:23:14 -0500

    Update hemoglobina.py
    ultimas modificacfiones

--- hemoglobina.py
+++ hemoglobina.py
@@ -2,6 +2,7 @@ homo = float(input())
 genero = int(input())
 
 if genero == 1 or genero == 2:
+    #si el genero es igual a 1 entrar a las alertas de masculino
     if genero == 1:
         if homo  <  13.2:
             print(f'Alerta 1')
@@ -9,6 +10,7 @@ if genero == 1 or genero == 2:
             print(f'Sin alerta')
         elif homo > 16.6:
             print(f'Alerta 2')
+    #si el genero es difrente a 1 entra a else de genero femenino
     else:
         if homo < 11.6:
             print(f'Alerta 1')
